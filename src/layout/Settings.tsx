754f5238c1a8620a86ee603da7b1fa584134b575
paul_carkner@hotmail.com <paul_carkner@hotmail.com>
2023-01-26 21:43:02 -0500

    Added clock

--- src/layout/Settings.tsx
+++ src/layout/Settings.tsx
@@ -7,7 +7,7 @@ Output: JSX.Element
 
 *****************/
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //Styles
 import styles from "./Settings.module.css";
@@ -15,6 +15,18 @@ import styles from "./Settings.module.css";
 //Type Declarations
 
 export const Settings = (): JSX.Element => {
+  const [t, setTime] = useState(new Date()); //create clock state
+
+  //update clock every second
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <div className={styles.Settings}>
       <button className={styles.Button}>
@@ -29,6 +41,13 @@ export const Settings = (): JSX.Element => {
       <button className={styles.Button}>
         <span className="material-symbols-sharp">settings</span>
       </button>
+      <div className={styles.Clock}>
+        {(((t.getHours() % 12) - 12) % 12) + 12}
+        {/* convert from 24-hour to 12 hour */}
+        <span className={styles.ClockColon}>:</span>
+        {("0" + t.getMinutes()).substr(-2)}
+        {/* make minutes two digits */} {t.getHours() > 11 ? "PM" : "AM"}
+      </div>
     </div>
   );
 };
