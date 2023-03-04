0e9acfbd55f96d85e4b6ee5bec6cdc627315423b
paul_carkner@hotmail.com <paul_carkner@hotmail.com>
2023-03-04 14:26:25 -0500

    Added retirement goal to finances dashboard

--- src/boards/FinancesDashboard.tsx
+++ src/boards/FinancesDashboard.tsx
@@ -1,7 +1,36 @@
 import React from "react";
 
+import boardStyles from "./BoardStyles.module.css";
+
+//Components
+import { Panel } from "./../layout/Panel";
+import { GoalChart } from "./../features/DataChart/DataChart";
+
 export class FinancesDashboard extends React.Component {
   render() {
-    return <div>finances</div>;
+    return (
+      <div className={boardStyles.Board}>
+        <div className={boardStyles.MiniPanelsContainer}>
+          <Panel title="Retirement Savings" info="JSON Data Chart">
+            <GoalChart
+              url="/sample_data/sample_bank_api.json"
+              labelsProcessor={(data: any) => "Vacation Savings"}
+              dataProcessor={(data: any) => {
+                return {
+                  value: data.accounts["0048394_485125813"].funds,
+                  valueText:
+                    "$" +
+                    data.accounts["0048394_485125813"].funds
+                      .toFixed(0)
+                      .replace(/(\d)(?=(\d{3})+$)/g, `$1,`),
+                  goal: 1000000,
+                  goalText: "$1,000,000",
+                };
+              }}
+            />
+          </Panel>
+        </div>
+      </div>
+    );
   }
 }
