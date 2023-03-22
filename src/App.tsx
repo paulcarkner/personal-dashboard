1c3c48cbf5492a44dae57e5eb531d8d6681f80c1
paulcarkner <paul.carkner@gmail.com>
2023-03-21 23:01:33 -0400

    Commenting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-//Import Features
+//Styles
+import styles from "./App.module.css";
+
+//Components
 import { Main } from "./layout/Main";
 import { SideBar } from "./layout/SideBar";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -8,9 +11,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 //Redux Imports
 import { useAppSelector } from "./app/hooks";
 import { isDarkSelector } from "./features/DarkMode/DarkModeSlice";
-
-//Styles
-import styles from "./App.module.css";
 
 function App(): JSX.Element {
   const isDark: boolean = useAppSelector(isDarkSelector);
